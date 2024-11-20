773fe05590ceba2c4c29f0930770dea962becae1
ChrispinSantacruz <christian.santacruzl@campusucc.edu.co>
2024-11-19 21:13:25 -0500

    frontend espera de conexion con base de datos

--- Infraestructura/APIS/usuarios/routes/orderRoutes.js
+++ Infraestructura/APIS/usuarios/routes/orderRoutes.js
@@ -1,20 +1,8 @@
-/**
- * Christian Santacruz 
- * 
- * David Inguilan
- * 
- * Cafe Arandia 2.0
- * 
- * Rutas Para Ordenes
- */
 const express = require('express');
-const { createOrder, updateOrder, deleteOrder, getOrders } = require('../controllers/orderController');
+const { createOrder, getOrders } = require('../controllers/orderController');
 const router = express.Router();
 
 router.post('/create', createOrder);
-router.put('/:id', updateOrder);      
-router.delete('/:id', deleteOrder);   
-router.get('/', getOrders);           
+router.get('/', getOrders);
 
 module.exports = router;
-
